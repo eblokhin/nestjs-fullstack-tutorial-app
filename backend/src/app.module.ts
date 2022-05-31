79f22bd326dec8da995ee76bef247da8a3a9af2b
Eugene Blokhin <mayson2@yandex.ru>
2022-05-31 11:27:10 +0700

    feat: add jwt, mongoose and static serve modules to app controller; enable cors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { secret } from './utils/constants';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
